0b1d279514b1b1d9bd2c8e31eba8d9da05456656
pratik-desai <pratikdesai204@gmail.com>
2026-08-21 16:44:38 +0530

    Update Host.java

--- Host.java
+++ Host.java
@@ -2,4 +2,9 @@ class Host{
     public static void main(String[] args){
         System.out.println("hello this is a new project !!");
     }
+    public static void message(){
+        System.out.println("Hello this is a new user1");
+    }
 }
+//yarr you are doing lot of work for me
+//you dont mind right? are you there? yes
