4f05fa42d94ee706f3b3fc0bfb2e1075ef6bbb63
SindhujaKammari <sindhukammari0@gmail.com>
2026-08-23 23:43:00 +0530

    generated logic for getting the movies based on the userInput room for more features

--- MovieDecision.java
+++ MovieDecision.java
@@ -1,9 +1,8 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.sql.*;
 
 public class MovieDecision {
-    static void GenreSelector(Map<String , List<String>> movName){
+    public static String GenreSelector(Map<String , List<String>> movName){
         HashMap<String,Integer> countTheGenre = new HashMap<>();
 
         for(Map.Entry<String, List<String>> genreName : movName.entrySet()){
@@ -24,7 +23,40 @@ public class MovieDecision {
                 resultGenre = resultVal.getKey();
             }
         }
-        System.out.println(" the final result:" + resultGenre);
-    }
+        System.out.println("Pick the movie:\n");
+        showMovies(resultGenre);
+        return resultGenre;
+    } 
+
+    public static void showMovies(String resultGenre){ // this is jdbc  this method shows the lst of movie acc to the input  o
+        try{
+            Connection con = dbConnection.getConnection(); 
+            String query = " Select * from movieData where Genre= ?"; //well get that value from here
+            PreparedStatement stmt = con.prepareStatement(query); // so this is sayin prepare our sql ready 
+            stmt.setString(1,resultGenre); // this is setting the value like we have 1 unknon value right? yes so we put setString(1,genreName); genreName wiill get from ? our yesyestrdays code dont remove this comments it will help me tomo okk 
+            /*
+            so if we have like this then we'll say  Select * from movieData where Genre= ? and moviename=?
+            setString(1,genreName);
+            setString(2,moviename); got it yes now  */
+            ResultSet rs = stmt.executeQuery(); // this is bascially saying execute the sql got it 
+            if(rs.next()){ //this is saying if we have the genre name in our db then do this
+                System.out.println("Genre: "+ resultGenre); // our genre
+                int i=1; // it is a counter 
+                while(rs.next()  && (i<=20)){ //rs.next() means so it is automatically sayng print everything from the picked out data 
+                    System.out.println(i+" "+rs.getString("MovieName")); // rs.getString("colName") it will print one by one until the last one hits yeah 
+                    i++;
+                }
+            }else{ // if we dont have the genre so tell the users this code 
     
+                System.out.println("Sorry this genre is very new to our Database\nbut we'll add the movies of the genre you requested as soon as possible");
+            }
+            
+
+        }catch(SQLException e){
+            System.out.println(e.getMessage()); // this means ik what tel me this is exceptionn handling it will print the line oddcurd error and reason right? yeah but see the diff 
+           
+        }
+        
+    }
+
 }
--- dbConnection.java
+++ dbConnection.java
@@ -1,18 +1,22 @@
 import java.sql.*; 
 
 public class dbConnection {
-    public static Connection connection(){
+    public static Connection getConnection(){
         try{
-            String url = System.getenv("DB_URL");
-            String username = System.getenv("DB_USERNAME");
-            String password = System.getenv("DB_PASSWORD");
+            String url = "OWN_URL"; // this is sql url its simple and you need to remember that part only
+            String username = "root"; // this is same for everyone 
+            String password = "OWN_PASSWORD"; // this is password set by you
 
-            Connection con = DriverManager.getConnection(url,username,password);
+            Connection con = DriverManager.getConnection(url,username,password); //
 
             return con;
         }catch(SQLException e){
            System.out.println("Couldnt connect to database");
-           System.out.println("Reason: "+e.getMessage());
+           System.out.println("Reason: "+e.getMessage());// we got simple message when we use e.getMessage got it
+           System.out.println("--------------------------------------------------------------------------");
+           e.printStackTrace(); /// when we use this  we get this huge disgusting block we cant even understand sometimes whats wrong 
+          // so i chose to get the message simply instead of this block okay? yes good nice over explanation what more do you want no everthing good  i want to see ow you study or learn things throu chat gpt can you show?i deleted it it was not logged it
+           System.out.println("*********************************************************************************");
            return null;
         }
         
