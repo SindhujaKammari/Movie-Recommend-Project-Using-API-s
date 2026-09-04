5accb38f348fd67f6f30e6125596b3cc7ccc4981
SindhujaKammari <sindhukammari0@gmail.com>
2026-09-04 21:31:18 +0530

    the problem is with the libraries and they dont work like usual

--- spring-learning/src/main/java/com/example/spring_learning/Course.java
+++ spring-learning/src/main/java/com/example/spring_learning/Course.java
@@ -1,17 +1,33 @@
 package com.example.spring_learning;
 
-import jakarta.validation.constraints.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
+@Entity 
+@Table(name = "courses")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     @Positive(message="Enter a proper Id")
     private int id;
 
+    @Column(name="courseName")
     @NotBlank(message="Enter a proper Course Name") 
     private String courseName;
 
+    @Column(name="titleDuration")
     @NotBlank(message="Enter a proper Title Duration")
     private String titleDuration;
 
+    @Column(name="price")
     @DecimalMin(value = "0.0", inclusive = true, message = "Price cannot be negative")
     private double price;
 
